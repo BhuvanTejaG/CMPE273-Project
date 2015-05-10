723dca228facb76bf946577b6e05a0f72428e449
DivyaBitragunta <divya.bitragunta@gmail.com>
2015-05-10 14:09:42 -0700

    Update StackOverFlowController.java

--- CourSuggest ver-2.0/src/main/java/cmpe273/StackOverFlowController.java
+++ CourSuggest ver-2.0/src/main/java/cmpe273/StackOverFlowController.java
@@ -1,5 +1,11 @@
 package cmpe273;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import java.util.ArrayList;
+
 @ComponentScan("model")
 @Controller
 @SessionAttributes("userObj")
@@ -19,8 +25,19 @@ public class StackOverFlowController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String stackOverFlowForm(Model model, StackOverFlow stackoverflow) {
         //this will be used for REST service - GET
-		
-        return NULL;
+	StackOverFlow stackOverFlow = new StackOverFlow();
+        /*
+         * Replaced this list with list of Stack Over Flow skills populate it.
+         * It will call with get request. Or when the url is called first time.
+         */
+        List<String> skills = new ArrayList<String>();
+        skills=stackOverFlowData();
+       /* skills.add("Java");
+        skills.add("Python");
+        skills.add("Scala");*/
+        stackOverFlow.setSkillList(skills);
+        model.addAttribute("stackOverFlow", stackOverFlow);
+        return "stackOverFlow";
 
     }
 
