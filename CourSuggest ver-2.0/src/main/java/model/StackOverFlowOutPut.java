1b4e2067b632f5a71e644a94bf6cf03f72b64196
DineshPandey2014 <dineshpandeysjsu@gmail.com>
2015-05-12 00:16:39 -0700

    Commit Model Class
    Commit Model Class For StackOveFlow.

---
+++ CourSuggest ver-2.0/src/main/java/model/StackOverFlowOutPut.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.util.List;
+
+public class StackOverFlowOutPut {
+
+    String skill;
+    List<Coursera> courses;
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+
+    public List<Coursera> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Coursera> courses) {
+        this.courses = courses;
+    }
+
+
+}
