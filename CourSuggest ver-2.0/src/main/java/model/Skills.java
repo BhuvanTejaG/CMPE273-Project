cc94df578e478f078c5ca0d0a52106cb757c13ee
DivyaBitragunta <divya.bitragunta@gmail.com>
2015-05-13 04:09:06 -0700

    updating model changes

---
+++ CourSuggest ver-2.0/src/main/java/model/Skills.java
@@ -0,0 +1,31 @@
+package model;
+
+
+import java.util.ArrayList;
+
+
+public class Skills {
+
+
+    private ArrayList<String> Skill;
+
+    public ArrayList<String> getSkill() {
+        return Skill;
+    }
+
+    public void setSkill(ArrayList<String> skill) {
+        Skill = skill;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    private String userId;
+
+
+
+}
