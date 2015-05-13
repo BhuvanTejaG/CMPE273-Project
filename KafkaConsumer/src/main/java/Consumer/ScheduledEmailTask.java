a46c607317353a1a7df45d8487a7c694f00ce998
DineshPandey2014 <dineshpandeysjsu@gmail.com>
2015-05-13 09:17:12 -0700

    commit

--- KafkaConsumer/src/main/java/Consumer/ScheduledEmailTask.java
+++ KafkaConsumer/src/main/java/Consumer/ScheduledEmailTask.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
+/**
+ * Created by saung on 4/7/15.
+ */
 @Component
 public class ScheduledEmailTask {
 
@@ -20,6 +23,8 @@ public class ScheduledEmailTask {
     @Scheduled(fixedRate = 1000)
     public void consumeMessages() {
         System.out.println("The time is now " + new Date());
-		//Stub Scheduler for calling Kafka consumer
+       // KafkaConsumer kafkaConsumer = new KafkaConsumer("54.153.29.218:2181", "test-group", "Course", emailService);
+        MessageConsumer kafkaConsumer = new MessageConsumer("Course");
+        kafkaConsumer.consume();
     }
 }
